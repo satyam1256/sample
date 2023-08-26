a74736e9ba9977b5de9788a17ce0d8a6f6593449
Satyam <99088607+satyam1256@users.noreply.github.com>
2023-08-26 17:09:08 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import TokenInfo from './design/index.js';
+import  TokenInfo from  './design/index.js';
+
+function App() {
+  return (
+    <div className="App">
+      <TokenInfo />
+    </div>
+  );
+}
+
+export default App;
